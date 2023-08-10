9c0de94c4a9afc0cd8043f5b65960d4183e189e2
David Naranjo <davidnaranjodev@gmail.com>
2023-08-10 12:20:33 +0100

    setting up project

---
+++ src/main/java/dev/dnaranjo/eOkami/EOkamiApplication.java
@@ -0,0 +1,13 @@
+package dev.dnaranjo.eOkami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EOkamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EOkamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dnaranjo/eOkami/EOkamiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dnaranjo.eOkami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EOkamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
